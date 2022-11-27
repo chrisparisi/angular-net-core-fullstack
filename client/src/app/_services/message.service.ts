3d6d1050ff737106fee804afffba31028cf96f45
Chris <the.chris.parisi@gmail.com>
2022-11-27 10:59:48 -0800

    added hub connection to message service

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../_models/message';
 import { User } from '../_models/user';
@@ -13,6 +14,8 @@ export class MessageService {
   baseUrl = environment.apiUrl;
   hubUrl = environment.hubUrl;
   private hubConnection?: HubConnection;
+  private messageThreadSource = new BehaviorSubject<Message[]>([]);
+  messageThread$ = this.messageThreadSource.asObservable();
 
   constructor(private http: HttpClient) {}
 
@@ -25,9 +28,15 @@ export class MessageService {
       .build();
 
     this.hubConnection.start().catch((error) => console.log(error));
+
+    this.hubConnection.on('ReceiveMessageThread', (messages) => {
+      this.messageThreadSource.next(messages);
+    });
   }
 
-  stopHubConnection() {}
+  stopHubConnection() {
+    this.hubConnection?.stop();
+  }
 
   getMessages(pageNumber, pageSize, container) {
     let params = getPaginationHeaders(pageNumber, pageSize);
