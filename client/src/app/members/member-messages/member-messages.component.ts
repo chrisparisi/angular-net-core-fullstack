9cf2df5b49aa2381e3472b936e9f64eb3ce10b7e
Chris <the.chris.parisi@gmail.com>
2022-12-01 18:11:05 -0800

    signalr updates

--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -10,11 +10,10 @@ import { MessageService } from 'src/app/_services/message.service';
 })
 export class MemberMessagesComponent implements OnInit {
   @ViewChild('messageForm') messageForm: NgForm;
-  @Input() messages: Message[];
-  @Input() username: string;
+  @Input() username?: string;
   messageContent: string;
 
-  constructor(private messageService: MessageService) {}
+  constructor(public messageService: MessageService) {}
 
   ngOnInit(): void {}
 
@@ -22,8 +21,8 @@ export class MemberMessagesComponent implements OnInit {
     this.messageService
       .sendMessage(this.username, this.messageContent)
       .subscribe((message) => {
-        this.messages.push(message);
-        this.messageForm.reset();
+        //this.messages.push(message);
+        //this.messageForm.reset();
       });
   }
 }
