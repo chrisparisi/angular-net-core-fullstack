3b7ac326c5f5b30a8ad465348e471e2376ebff51
Chris <the.chris.parisi@gmail.com>
2022-12-11 15:37:25 -0800

    added Connection class

--- API/Entities/Group.cs
+++ API/Entities/Group.cs
@@ -8,7 +8,13 @@ namespace API.Entities
 {
     public class Group
     {
+        public Group(string name)
+        {
+            Name = name;
+        }
+
         [Key]
         public string Name { get; set; }
+        public ICollection<Connection> Connections { get; set; } = new List<Connection>();
     }
 }
