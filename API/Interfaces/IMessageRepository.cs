519c8592748f6688494a44af29cd5f25bd64ea49
Chris <the.chris.parisi@gmail.com>
2022-12-14 16:01:32 -0800

    finished imessage repo

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -18,5 +18,7 @@ namespace API.Interfaces
         Task<bool> SaveAllAsync();
         void AddGroup(Group group);
         void RemoveConnection(Connection connection);
+        Task<Connection> GetConnection(string connectionId);
+        Task<Group> GetMessageGroup(string groupName);
     }
 }
