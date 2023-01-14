ac84b2ed69591062a2c902dd3cc23b0af5a7da93
Chris <the.chris.parisi@gmail.com>
2023-01-13 19:55:50 -0800

    update

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,7 @@ namespace API
                 var userManager = services.GetRequiredService<UserManager<AppUser>>();
                 var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
                 await context.Database.MigrateAsync();
+                context.Connections.RemoveRange(context.Connections);
                 await Seed.SeedUsers(userManager, roleManager);
             }
             catch (Exception ex)
