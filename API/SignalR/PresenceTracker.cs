b268560c756c12be6e793615476e781b03d76e76
Chris <the.chris.parisi@gmail.com>
2023-01-18 14:17:14 -0800

    update

--- API/SignalR/PresenceTracker.cs
+++ API/SignalR/PresenceTracker.cs
@@ -53,5 +53,10 @@ namespace API.SignalR
 
             return Task.FromResult(onlineUsers);
         }
+
+        public static Task<List<string>> GetConnectionsForUser(string username) 
+        {
+            List<string> connectionIds;
+        }
     }
 }
